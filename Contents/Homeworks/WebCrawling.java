d1d3b83089e6231701c67ee0d34ca17d9d062658
NoobTh <n006h3417h@gmail.com>
2024-09-12 14:23:08 +0900

    Update WebCrawling.java

--- Contents/Homeworks/WebCrawling.java
+++ Contents/Homeworks/WebCrawling.java
@@ -1,19 +1,34 @@
-import java.io.IOException;
-import java.util.Scanner;
+// GeekNews Parser
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Scanner;
 
 class WebCrawling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        String url = scanner.nextLine();
-        
-        String url = "";
-        
+        String url = "https://news.hada.io/";
+        System.out.println("[GeekNews]");
+        try {
+            Document page = Jsoup.connect(url).get();
+            Elements topics = page.getElementsByClass("topic_row");
+            for (Element topic : topics) {
+            	Elements topictitle = topic.getElementsByClass("topictitle");
+            	String title = topic.getElementsByTag("h1").text();
+            	String topicUrl = topictitle.get(0).getElementsByTag("a").attr("href");
+            	System.out.println("- " + title);
+            	System.out.println("  " + topicUrl + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         scanner.close();
     }
 }
 
+      
