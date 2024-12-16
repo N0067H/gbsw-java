da22fc41c76af470b7728be77b2b9e04a4c8bbe1
SeungYup Ryu <n006h3417h@gmail.com>
2024-12-16 20:26:26 +0900

    Upload

--- Contents/OOP/chapter1/AddressApp.java
+++ Contents/OOP/chapter1/AddressApp.java
@@ -1,12 +1,13 @@
 package chapter1;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressApp {
 
     public static void main(String[] args) {
-        var list = new ArrayList<Friend>();
+        var list = load();
         var in = new Scanner(System.in);
 
         int menu = 0;
@@ -17,10 +18,23 @@ public class AddressApp {
             System.out.println("* 3. Remove User           *");
             System.out.println("* 4. Program Exit          *");
             System.out.println("============================");
+
             menu = in.nextInt();
 
-            switch (menu) {
-            case 2:
+            if (menu == 1) {
+                System.out.print("Name: ");
+                var name = in.next();
+
+                for (var f : list) {
+                    if (f.getName().contains(name)) {
+                        System.out.println(f.getName() + '\t'
+                                + f.getPhone() + '\t'
+                                + f.getNick() + '\t'
+                                + f.getCode() + '\t'
+                                + f.getAge() + '\n');
+                    }
+                }
+            } else if (menu == 2) {
                 System.out.print("Name: ");
                 var name = in.next();
 
@@ -38,12 +52,51 @@ public class AddressApp {
 
                 list.add(new Friend(name, phone, nick, code, age));
                 System.out.println("User added");
-                
-            default:
+            } else if (menu == 3) {
+                System.out.print("Name: ");
+                var name = in.next();
+
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).getName().equals(name)) {
+                        list.remove(i);
+                        System.out.println("User removed");
+                        break;
+                    }
+                }
+            } else if (menu == 4) {
+                save(list);
+                System.exit(0);
+            } else {
                 System.out.println("Wrong menu");
             }
         }
 
         System.out.println("Program exited");
+    }
+
+    private static ArrayList<Friend> load() {
+        var list = new ArrayList<Friend>();
+
+        try (var in = new BufferedReader(new FileReader("friends.txt"))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                var parts = line.split("\t");
+                if (parts.length == 5) {
+                    var friend = new Friend(parts[0], parts[1], parts[2], parts[3], Integer.parseInt(parts[4]));
+                    list.add(friend);
+                }
+            }
+        } catch (IOException e) {
+        }
+        return list;
+    }
+
+    private static void save(ArrayList<Friend> list) {
+        try (var out = new BufferedWriter(new FileWriter("friends.txt"))) {
+            for (var f : list) {
+                out.write(f.getName() + "\t" + f.getPhone() + "\t" + f.getNick() + "\t" + f.getCode() + "\t" + f.getAge() + "\n");
+            }
+        } catch (IOException e) {
+        }
     }
 }
